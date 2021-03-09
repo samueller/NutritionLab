dd6ffbd5d1846bf093bef3f51b81e217efa5b12c
kenmueller <kenmueller0@gmail.com>
2021-03-08 22:35:29 -0800

    bullet graphs for biomarkers

--- NutritionLabSurvey/Components/BulletGraph.swift
+++ NutritionLabSurvey/Components/BulletGraph.swift
@@ -8,8 +8,8 @@ struct BulletGraph: View {
 	
 	let width: CGFloat
 	
-	let range: ClosedRange<Int>
-	let value: Int
+	let range: ClosedRange<Float>
+	let value: Float
 	
 	let units: String
 	
@@ -19,59 +19,71 @@ struct BulletGraph: View {
 	}
 	
 	var position: CGFloat {
-		width * .init(value) / .init(range.upperBound)
+		width * .init(value / range.upperBound)
+	}
+	
+	var scale: some View {
+		HStack(spacing: 0) {
+			Rectangle()
+				.foregroundColor(.orange)
+			Rectangle()
+				.foregroundColor(.green)
+			Rectangle()
+				.foregroundColor(.orange)
+		}
+		.opacity(0.7)
+		.frame(height: Self.HEIGHT)
+		.cornerRadius(Self.HEIGHT / 2)
+	}
+	
+	var numbers: some View {
+		HStack {
+			ForEach(0..<Self.SEGMENTS + 1) { position in
+				Text(String(range.upperBound * .init(position) / .init(Self.SEGMENTS)))
+				if position < Self.SEGMENTS { Spacer() }
+			}
+		}
+	}
+	
+	var overlay: some View {
+		ZStack(alignment: .topLeading) {
+			HStack {
+				Text(String(value))
+					.font(.title)
+					.bold()
+					.foregroundColor(ok ? .green : .red)
+				Text(units)
+					.foregroundColor(.gray)
+			}
+			.padding(.horizontal, 12)
+			.padding(.vertical, 8)
+			.background(Color(ok ? #colorLiteral(red: 0.862745098, green: 0.9529411765, blue: 0.8784313725, alpha: 1) : #colorLiteral(red: 0.968627451, green: 0.8078431373, blue: 0.8039215686, alpha: 1)))
+			.cornerRadius(100)
+			.offset(x: -20, y: -70)
+			.zIndex(1)
+			Rectangle()
+				.foregroundColor(ok ? .green : .red)
+				.frame(width: 2, height: 40)
+				.offset(x: Self.RADIUS - 1, y: Self.RADIUS - 40)
+			Circle()
+				.foregroundColor(ok ? .green : .red)
+				.padding(3)
+				.overlay(
+					Circle()
+						.stroke(ok ? Color.green : Color.red, lineWidth: 1)
+				)
+				.frame(width: Self.RADIUS * 2, height: Self.RADIUS * 2)
+		}
+		.offset(x: position - Self.RADIUS, y: Self.HEIGHT / 2 - Self.RADIUS)
 	}
 	
 	var body: some View {
 		ZStack(alignment: .topLeading) {
 			VStack {
-				HStack(spacing: 0) {
-					Rectangle()
-						.foregroundColor(.orange)
-					Rectangle()
-						.foregroundColor(.green)
-					Rectangle()
-						.foregroundColor(.orange)
-				}
-				.opacity(0.7)
-				.frame(height: Self.HEIGHT)
-				.cornerRadius(Self.HEIGHT / 2)
-				HStack {
-					ForEach(0..<Self.SEGMENTS + 1) { position in
-						Text(String(range.upperBound * position / Self.SEGMENTS))
-						if position < Self.SEGMENTS { Spacer() }
-					}
-				}
+				scale
+				numbers
 			}
-			ZStack(alignment: .topLeading) {
-				HStack {
-					Text(String(value))
-						.font(.title)
-						.bold()
-						.foregroundColor(ok ? .green : .red)
-					Text(units)
-						.foregroundColor(.gray)
-				}
-				.padding(.horizontal, 12)
-				.padding(.vertical, 8)
-				.background(Color(ok ? #colorLiteral(red: 0.862745098, green: 0.9529411765, blue: 0.8784313725, alpha: 1) : #colorLiteral(red: 0.968627451, green: 0.8078431373, blue: 0.8039215686, alpha: 1)))
-				.cornerRadius(100)
-				.offset(x: -20, y: -70)
-				.zIndex(1)
-				Rectangle()
-					.foregroundColor(ok ? .green : .red)
-					.frame(width: 2, height: 40)
-					.offset(x: Self.RADIUS - 1, y: Self.RADIUS - 40)
-				Circle()
-					.foregroundColor(ok ? .green : .red)
-					.padding(3)
-					.overlay(
-						Circle()
-							.stroke(ok ? Color.green : Color.red, lineWidth: 1)
-					)
-					.frame(width: Self.RADIUS * 2, height: Self.RADIUS * 2)
-			}
-			.offset(x: position - Self.RADIUS, y: Self.HEIGHT / 2 - Self.RADIUS)
+			overlay
 		}
 		.frame(width: width)
 	}
